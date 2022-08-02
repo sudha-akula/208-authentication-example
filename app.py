5364ab92d640337ec3a85a484c9e771386f58810
sudha-akula <sudha_akula@intuit.com>
2022-08-02 03:17:48 +0000

    create bubble charts for each country

--- app.py
+++ app.py
@@ -3,35 +3,69 @@ import dash_auth
 from dash import html, dcc
 from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
+import plotly.express as px
+import math
 
 
 # Keep this out of source code repository - save in a file or a database
 VALID_USERNAME_PASSWORD_PAIRS = {
-    'Mickey': 'Mouse', 'Donald': 'Duck'
+    'Mickey': 'Mouse', 'Donald': 'Duck', 'Sudha': 'Akula'
 }
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
-app.title='auth example'
+app.title='auth_plus_charts'
 auth = dash_auth.BasicAuth(
     app,
     VALID_USERNAME_PASSWORD_PAIRS
 )
+
+# Dictionary with dataframes for each continent
+continent_names = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania']
+
+
+# get data from plotly express
+df = px.data.gapminder()
+df_2007 = df[df['year']==2007]
+df_2007 = df_2007.sort_values(['continent', 'country'])
+
+hover_text = []
+bubble_size = []
+
+for index, row in df_2007.iterrows():
+    hover_text.append(('Country: {country}<br>'+
+                      'Life Expectancy: {lifeExp}<br>'+
+                      'GDP per capita: {gdp}<br>'+
+                      'Population: {pop}<br>'+
+                      'Year: {year}').format(country=row['country'],
+                                            lifeExp=row['lifeExp'],
+                                            gdp=row['gdpPercap'],
+                                            pop=row['pop'],
+                                            year=row['year']))
+    bubble_size.append(math.sqrt(row['pop']))
+    
+
+df_2007['text'] = hover_text
+df_2007['size'] = bubble_size
+sizeref = 2.*max(df_2007['size'])/(100**2)
+
 
 app.layout = html.Div([
     html.H1('Welcome to the app'),
     html.H3('You are successfully authorized'),
     dcc.Dropdown(
         id='dropdown',
-        options=[{'label': i, 'value': i} for i in [1, 2, 3, 4, 5]],
-        value=1
+        #options=[{'label': i, 'value': i} for i in [1, 2, 3, 4, 5]],
+        options=[{'label': i, 'value': i} for i in continent_names],
+        value='Africa'
     ),
 
     html.Div(id='graph-title'),
     dcc.Graph(id='graph'),
-    html.A('Code on Github', href='https://github.com/austinlasseter/dash-auth-example'),
+    html.A('Auth Code on Github', href='https://github.com/austinlasseter/dash-auth-example'),
+    html.A('Plotly Code on Github', href='https://plotly.com/python/bubble-charts/'),
     html.Br(),
     html.A("Data Source", href='https://dash.plotly.com/authentication'),
 ], className='container')
@@ -43,29 +77,41 @@ app.layout = html.Div([
     )
 def update_graph(dropdown_value):
 
-    x_values = [-3,-2,-1,0,1,2,3]
-    y_values = [x**dropdown_value for x in x_values]
-    colors=['black','red','green','blue','orange','purple']
-    graph_title='Graph of {}'.format(str(dropdown_value))
+#     x_values = [-3,-2,-1,0,1,2,3]
+#     y_values = [x**dropdown_value for x in x_values]
+#     colors=['black','red','green','blue','orange','purple']
+    graph_title='Bubble Chart of {}'.format(str(dropdown_value))
 
 
-    trace0 = go.Scatter(
-        x = x_values,
-        y = y_values,
-        mode = 'lines',
-        marker = {'color': colors[dropdown_value]},
-    )
+    fig = px.scatter(df_2007.query("continent==2007"), x="gdpPercap", y="lifeExp",
+	         size="pop", color="continent",
+                 hover_name="country", log_x=True, size_max=60)
+    
+    fig = px.scatter(df_2007.query("continent=='Europe'"), x="gdpPercap", y="lifeExp",
+	         size="pop",color="iso_alpha",
+                 hover_name="country", log_x=True, size_max=60)
+    return graph_title, fig
+    # fig.show()
+
+
+
+    # trace0 = go.Scatter(
+    #     x = x_values,
+    #     y = y_values,
+    #     mode = 'lines',
+    #     marker = {'color': colors[dropdown_value]},
+    # )
 
     # assign traces to data
-    data = [trace0]
-    layout = go.Layout(
-        title = graph_title
-    )
+    # data = [trace0]
+    # layout = go.Layout(
+    #     title = graph_title
+    #)
 
     # Generate the figure dictionary
-    fig = go.Figure(data=data,layout=layout)
+#     fig = go.Figure(data=data,layout=layout)
 
-    return graph_title, fig
+#     return graph_title, fig
 
 
 ############ Deploy
